8484302656112d2ca495a59e3c686c9095bd50cb
Donnel Mathew <donnel@matt-lnx.attlocal.net>
2020-07-13 22:12:08 -0700

    My first C program

---
+++ hello.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main ()
+{
+    printf ("\nMy First c progam\n\n");
+    printf ("\nBy Donnel\n\n");
+    printf ("\nMy laptop\n");
+    printf ("\nThe End!!!\n");
+    printf ("\nHello my name is Donnel\n");
+    printf ("\nBy Donnel\n");
+    printf ("\nThe End\n");
+    printf ("\nMy name is Donnel\n");
+    printf ("\nThis is my first c progam\n");
+    printf ("\nThis is Donnel\n");
+    printf ("\nThis is my first c progam\n");
+    printf ("\nMy name is Donnel\n");
+    printf ("\nThis is my laptop\n");
+    printf ("\nc progam is difficult\n");
+    printf ("\nc progam is so much typing and learning\n");
+    printf ("\nc progam is fun t\n");
+    printf ("\nMy dad has learned c progam and so much other stuff\n");
+    printf ("\nI am 7 years old and almost 8\n");
+
+    
+
+    
+ 
+    return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
